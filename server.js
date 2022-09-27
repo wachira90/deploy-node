0abda5b5c0cdcde3d59f8acc22c25718246b2011
Wachira Duangdee <wcrcorp@gmail.com>
2022-09-27 17:37:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, '0.0.0.0');
+}).listen('5011', '0.0.0.0');
 console.log('Server running at http://localhost:8080/');
