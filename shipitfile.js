a508bce2c4673acd77b439e86e6dc02f78c1a22a
Wachira Duangdee <wcrcorp@gmail.com>
2022-09-28 01:52:10 +0700

    Update shipitfile.js

--- shipitfile.js
+++ shipitfile.js
@@ -1,6 +1,18 @@
+
 module.exports = shipit => {
 	// Load shipit-deploy tasks
 	require('shipit-deploy')(shipit)
+
+
+	// shipit.on('updated', async () => {
+	// 	shipit.start('npm-install', 'copy-config', 'copy-config2', 'copy-config3', 'permission-config');
+	// });
+
+	shipit.on('published', async () => {
+		shipit.start('pm2-server');
+	});
+
+
 
 	shipit.initConfig({
 		default: {
@@ -12,4 +24,10 @@ module.exports = shipit => {
 			servers: 'ubuntu@172.16.1.5'
 		},
 	})
+
+	shipit.blTask('pm2-server', async () => {
+		await shipit.remote('yarn start');
+	});
+
+
 }
