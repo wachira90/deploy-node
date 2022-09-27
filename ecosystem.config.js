5b554fc2f6d41bf9deb98daf21a25a1ee36474f0
Wachira Duangdee <wcrcorp@gmail.com>
2022-09-28 01:23:21 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+ module.exports = {
+              apps: [
+                {
+                  name: 'test-node',
+                  script: 'server.js',
+                  watch: true,
+                  autorestart: true,
+                  restart_delay: 1000,
+                  env: {
+                    NODE_ENV: 'development'
+                  },
+                  env_production: {
+                    NODE_ENV: 'production'
+                  }
+                }
+              ]
+            };
